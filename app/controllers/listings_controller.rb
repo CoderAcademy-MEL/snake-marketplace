3b3d1f43d1988ad00a5c936eed303cd6d6c8e9ac
Harrison Malone <harrison.malone2@gmail.com>
2020-04-27 22:39:37 +1000

    much nicer front end

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -15,7 +15,7 @@ class ListingsController < ApplicationController
   def create
     @listing = Listing.new(listing_params)
     if @listing.save
-      redirect_to root_path
+      redirect_to @listing
     else
       render :new
     end 
--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,10 @@
 module ListingsHelper
+  def background_image(listing)
+    return "
+      background: url(#{url_for(listing.picture)});
+      height: 100%; 
+      background-position: center; 
+      background-size: cover;
+    "
+  end
 end
