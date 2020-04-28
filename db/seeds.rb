4d2a886cd21a6a57bbe86a6307be14417d8daca9
Harrison Malone <harrison.malone2@gmail.com>
2020-04-28 11:37:32 +1000

    added seed file logic for listings and images

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,36 @@ Snake.names.each do |name|
   Breed.create(name: name.downcase)
   puts "#{name} created"
 end
+
+snakes = [
+  {
+    title: "conbra kai",
+    description: "really scary snake",
+    breed_id: 29
+  },
+  {
+    title: "toby",
+    description: "an awesome boy",
+    breed_id: 117
+  },
+  {
+    title: "glen",
+    description: "a beautiful boy",
+    breed_id: 50
+  },
+  {
+    title: "jake",
+    description: "my name rhymes ",
+    breed_id: 16
+  }
+]
+
+snakes.each do |snake|
+  random_num = rand(1..3)
+  listing = Listing.create(snake)
+  listing.picture.attach(
+    io: File.open("app/assets/images/snake-#{random_num}.jpg"), 
+    filename: "snake-#{random_num}.jpg", 
+    content_type: "image/jpg"
+  )
+end 
