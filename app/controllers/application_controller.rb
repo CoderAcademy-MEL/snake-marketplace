4577a1803b806cafacdab0259a0b73fb737f0e6b
Harrison Malone <harrison.malone2@gmail.com>
2020-04-30 14:30:34 +1000

    spaces changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,5 @@
 class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
-<<<<<<< HEAD
-    flash[:authorization_error] = "Not authorized to perform that action ❌"
-    redirect_to listings_path
-  end 
-end
-=======
     render file: "#{Rails.root}/public/403.html" , status: 403
   end
 end
->>>>>>> 1a12abd26f8d15c38de83db6093a292144171a44
