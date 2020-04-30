da95d80c4b8363cb765f337d2874277e0ba1f3fc
Harrison Malone <harrison.malone2@gmail.com>
2020-04-30 11:22:33 +1000

    added User.new logic to ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,6 +4,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    user ||= User.new
     can [:index, :show, :new, :create], Listing
     can [:edit, :update, :destroy], Listing, user_id: user.id
   end
