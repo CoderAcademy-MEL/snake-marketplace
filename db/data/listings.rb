512aae8f07e5250e8c518ed641e5561e5b28417c
Harrison Malone <harrison.malone2@gmail.com>
2020-05-05 09:34:30 +1000

    changed seed to have sold attribute on listings

--- db/data/listings.rb
+++ db/data/listings.rb
@@ -5,28 +5,32 @@ def snakes
       description: "really scary snake",
       breed_id: 29,
       user_id: 1,
-      deposit: 10000
+      deposit: 10000,
+      sold: false
     },
     {
       title: "toby",
       description: "an awesome boy",
       breed_id: 117,
       user_id: 1,
-      deposit: 20000
+      deposit: 20000,
+      sold: false
     },
     {
       title: "glen",
       description: "a beautiful boy",
       breed_id: 50,
       user_id: 1,
-      deposit: 5000
+      deposit: 5000,
+      sold: false
     },
     {
       title: "jake",
       description: "my name rhymes",
       breed_id: 16,
       user_id: 1,
-      deposit: 30000
+      deposit: 30000,
+      sold: false
     }
   ]
 end
