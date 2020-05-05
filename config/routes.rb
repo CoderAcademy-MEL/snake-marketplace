88dd49e0d53984c3fc154ce02c3e3958c247cddc
Harrison Malone <harrison.malone2@gmail.com>
2020-05-05 10:17:09 +1000

    added cart routes create and index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :listings
+  resources :carts, only: [:create, :index]
   get "/payments/session", to: "payments#get_stripe_id"
   get "/payments/success", to: "payments#success"
   post "/payments/webhook", to: "payments#webhook"
