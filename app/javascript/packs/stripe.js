0655f823ab42324f257d1ef85b5d0c15ee502efd
Harrison Malone <harrison.malone2@gmail.com>
2020-05-18 11:51:33 +1000

    accessing listings through active record instead of passing through params

--- app/javascript/packs/stripe.js
+++ app/javascript/packs/stripe.js
@@ -3,9 +3,8 @@
 
  const button = document.getElementById("stripe")
  button.addEventListener("click", (e) => {
-   const id = location.pathname.split("/")[2]
   // making a GET request through javascript   
-   fetch(`/payments/session?id=${id}`)
+   fetch(`/payments/session`)
    .then((res) => res.json())
    .then((data) => {
      const stripe = Stripe(data.stripe_public_key);
