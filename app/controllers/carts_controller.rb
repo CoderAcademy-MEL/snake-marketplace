61e83fcd8207bd8c382ffb5f1b1613aa0fa29d1e
Harrison Malone <harrison.malone2@gmail.com>
2020-05-18 11:52:20 +1000

    added logic to carts controller to ensure that only the last cart is used as the current cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,17 +1,20 @@
 class CartsController < ApplicationController
   def index 
-    if user_signed_in? && current_user.cart
-      @cart = current_user.cart.listings
+    if user_signed_in? && current_user.carts.last
+      @cart = current_user.carts.last.listings
+      @listing_ids = @cart.map do |listing|
+        listing.id
+      end
     else
       redirect_to listings_path
     end 
   end 
   
   def create
-    if !current_user.cart
+    if current_user.carts.empty?
       cart = Cart.create(completed: false, user_id: current_user.id)
     else
-      cart = current_user.cart
+      cart = current_user.carts.last
     end 
     listing = Listing.find(params[:listing_id])
     cart.listings << listing
